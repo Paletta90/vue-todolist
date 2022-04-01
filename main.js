a9421def4936fa08a4f175572e98370a23df96a2
DavideDiPietro <davide.dipietro3@gmail.com>
2022-04-01 14:59:29 +0200

    Millestone 1 - 2 - 3

--- main.js
+++ main.js
@@ -1,26 +1,46 @@
-var app = new Vue(
-    {
-        el: '#root',
+var app = new Vue({
 
-        data:{
-            arrayTodo: [
-                {
-                    textTodo: 'Cosa da fare 1',
-                    done: false
-                },
-                {
-                    textTodo: 'Cosa da fare 2',
-                    done: false
-                },
-                {
-                    textTodo: 'Cosa da fare 3',
-                    done: false
-                }
-            ],
+    el: '#root',
+
+    data: {
+
+        // Array di cose da fare
+        arrayTodo: [],
+
+        // Stringa dove scriverò la todo da stampare
+        todoText: ''
+    },
+
+    methods: {
+
+        //Cancello il singolo todo
+        deleteTodo: function (index) {
+            this.arrayTodo.splice(index, 1);
         },
 
-        methods: {
+        //Stampo la todo
+        printTodo: function (todoText) {
+
+            if (todoText != '') {
+                let newTodo = {
+                    textTodo: todoText,
+                    done: false
+                }
+                // Creo un nuovo oggetto todo
+                this.arrayTodo.push(newTodo);
+            }
+
+        },
+
+        // Sottolineo la todo al click e la tolgo ad un secondo click
+        doneTodo(index){
+
+            if( this.arrayTodo[index].done == false ){
+                this.arrayTodo[index].done = true
+            }else{
+                this.arrayTodo[index].done = false;
+            }
 
         }
     }
-)
+})
