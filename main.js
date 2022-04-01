6dd798ba47e58224cb87e84665dc44323e6bbd86
DavideDiPietro <davide.dipietro3@gmail.com>
2022-04-01 15:15:46 +0200

    Bonus 2 e Extra

--- main.js
+++ main.js
@@ -21,7 +21,13 @@ var app = new Vue({
         //Stampo la todo
         printTodo: function (todoText) {
 
-            if (todoText != '') {
+            if (todoText == '') {
+                alert("Non puoi inserire una todo vuota")
+
+            } else if (this.checkNewTodo(todoText)) {
+                alert("Hai già inserito questa todo")
+
+            } else {
                 let newTodo = {
                     textTodo: todoText,
                     done: false
@@ -30,17 +36,30 @@ var app = new Vue({
                 this.arrayTodo.push(newTodo);
             }
 
+            this.todoText = ''
+
         },
+        // Controllo se ho già inserito quasta todo
+        checkNewTodo(todoText) {
 
+            if (this.arrayTodo.some(e => e.textTodo == todoText)) {
+                return true;
+            } else {
+                return false;
+            }
+
+        },
         // Sottolineo la todo al click e la tolgo ad un secondo click
-        doneTodo(index){
+        doneTodo(index) {
 
-            if( this.arrayTodo[index].done == false ){
+            if (this.arrayTodo[index].done == false) {
                 this.arrayTodo[index].done = true
-            }else{
+            } else {
                 this.arrayTodo[index].done = false;
             }
 
-        }
+        },
+
+
     }
 })
