2e761819f6c643458272ef1e764ec454873e031e
DavideDiPietro <davide.dipietro3@gmail.com>
2022-04-01 14:29:14 +0200

    Creo il mockup

---
+++ main.js
@@ -0,0 +1,26 @@
+var app = new Vue(
+    {
+        el: '#root',
+
+        data:{
+            arrayTodo: [
+                {
+                    textTodo: 'Cosa da fare 1',
+                    done: false
+                },
+                {
+                    textTodo: 'Cosa da fare 2',
+                    done: false
+                },
+                {
+                    textTodo: 'Cosa da fare 3',
+                    done: false
+                }
+            ],
+        },
+
+        methods: {
+
+        }
+    }
+)
